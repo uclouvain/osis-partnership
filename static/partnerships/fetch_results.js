2fcfa436dadd00581ccd683d4c3b9c9479f8ae1a
Gagaro <gagaro42@gmail.com>
2018-06-11 11:58:06 +0200

    filter form in ajax

--- static/partnerships/fetch_results.js
+++ static/partnerships/fetch_results.js
@@ -1,13 +1,24 @@
 (function ($) {
-    $(document).ready(function() {
-        $(document).on('click', 'a.ajax-results', function(event) {
-            event.preventDefault();
-            event.stopPropagation();
+    // Pagination/ordering in AJAX
+    $(document).on('click', 'a.ajax-results', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
 
-            var url = $(this).attr('href');
-            $.get(url, function(data) {
-                $('#results').html(data);
-            });
+        var url = $(this).attr('href');
+        $.get(url, function(data) {
+            $('#results').html(data);
+        });
+    });
+
+    // Filter form submit in AJAX
+    $(document).on('submit', 'form#filter-form', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        var $form = $(this);
+
+        var url = $form.attr('action') + '?' + $form.serialize();
+        $.get(url, function(data) {
+            $('#results').html(data);
         });
     });
 })(jQuery);
