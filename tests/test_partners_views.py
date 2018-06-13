85b9e0061d4f444462cf119127b8bb00d87099e1
Gagaro <gagaro42@gmail.com>
2018-06-13 10:07:14 +0200

    test partners list

--- tests/test_partners_views.py
+++ tests/test_partners_views.py
@@ -9,18 +9,32 @@ class PartnersListView(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        PartnerFactory()
-        PartnerFactory()
-        PartnerFactory()
-        PartnerFactory()
+        for i in range(29):
+            PartnerFactory()
+        cls.partner_erasmus = PartnerFactory(erasmus_code='ZZZ')
         cls.user = UserFactory()
         cls.url = reverse('partnerships:partners:list')
 
     def test_get_list_anonymous(self):
-        response = self.client.get(self.url)
+        response = self.client.get(self.url, follow=True)
         self.assertTemplateNotUsed(response, 'partnerships/partners_list.html')
+        self.assertTemplateUsed(response, 'registration/login.html')
 
     def test_get_list_authenticated(self):
         self.client.force_login(self.user)
         response = self.client.get(self.url)
         self.assertTemplateUsed(response, 'partnerships/partners_list.html')
+
+    def test_get_list_pagination(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.url + '?page=2')
+        self.assertTemplateUsed(response, 'partnerships/partners_list.html')
+        context = response.context_data
+        self.assertEqual(len(context['partners']), 10)
+
+    def test_get_list_ordering(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.url + '?ordering=-erasmus_code')
+        self.assertTemplateUsed(response, 'partnerships/partners_list.html')
+        context = response.context_data
+        self.assertEqual(context['partners'][0], self.partner_erasmus)
