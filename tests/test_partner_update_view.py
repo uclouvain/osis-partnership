a5f7beadf9a4ed089d5ae2626f565aefcf00db69
Gagaro <gagaro42@gmail.com>
2018-06-21 15:03:12 +0200

    tests partner update view

--- tests/test_partner_update_view.py
+++ tests/test_partner_update_view.py
@@ -6,7 +6,7 @@ from base.tests.factories.entity_version import EntityVersionFactory
 from base.tests.factories.person_entity import PersonEntityFactory
 from base.tests.factories.user import UserFactory
 from partnership.models import ContactType
-from partnership.tests.factories import PartnerTagFactory, PartnerTypeFactory
+from partnership.tests.factories import PartnerFactory, PartnerTagFactory, PartnerTypeFactory
 from reference.tests.factories.country import CountryFactory
 
 
@@ -14,36 +14,56 @@ class PartnerCreateViewTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
+        # User creation
         cls.user = UserFactory()
         cls.user_adri = UserFactory()
         entity_version = EntityVersionFactory(acronym='ADRI')
         PersonEntityFactory(entity=entity_version.entity, person__user=cls.user_adri)
         cls.user_gf = UserFactory()
-        EntityManagerFactory(person__user=cls.user_gf)
+        entity_manager = EntityManagerFactory(person__user=cls.user_gf)
+        cls.user_other_gf = UserFactory()
+        EntityManagerFactory(person__user=cls.user_other_gf, entity=entity_manager.entity)
+        # Partner creation
+        cls.partner = PartnerFactory()
+        cls.partner_gf = PartnerFactory(author=cls.user_gf)
+        # Misc
         cls.contact_type = ContactType.objects.create(value='foobar')
         cls.country = CountryFactory()
-        cls.url = reverse('partnerships:partners:create')
+        cls.url = reverse('partnerships:partners:update', kwargs={'pk': cls.partner.pk})
 
-    def test_get_view_anonymous(self):
+    def test_get_partner_as_anonymous(self):
         response = self.client.get(self.url, follow=True)
-        self.assertTemplateNotUsed(response, 'partnerships/partner_create.html')
+        self.assertTemplateNotUsed(response, 'partnerships/partner_update.html')
         self.assertTemplateUsed(response, 'registration/login.html')
 
-    def test_get_view_authenticated(self):
+    def test_get_partner_as_authenticated(self):
         self.client.force_login(self.user)
         response = self.client.get(self.url, follow=True)
-        self.assertTemplateNotUsed(response, 'partnerships/partner_create.html')
+        self.assertTemplateNotUsed(response, 'partnerships/partner_update.html')
         self.assertTemplateUsed(response, 'registration/login.html')
 
-    def test_get_view_as_adri(self):
+    def test_get_partner_as_adri(self):
         self.client.force_login(self.user_adri)
         response = self.client.get(self.url)
-        self.assertTemplateUsed(response, 'partnerships/partner_create.html')
+        self.assertTemplateUsed(response, 'partnerships/partner_update.html')
 
-    def test_get_view_as_gf(self):
+    def test_get_other_partner_as_gf(self):
         self.client.force_login(self.user_gf)
-        response = self.client.get(self.url)
-        self.assertTemplateUsed(response, 'partnerships/partner_create.html')
+        response = self.client.get(self.url, follow=True)
+        self.assertTemplateNotUsed(response, 'partnerships/partner_update.html')
+        self.assertTemplateUsed(response, 'registration/login.html')
+
+    def test_get_own_partner_as_gf(self):
+        self.client.force_login(self.user_gf)
+        url = reverse('partnerships:partners:update', kwargs={'pk': self.partner_gf.pk})
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'partnerships/partner_update.html')
+
+    def test_get_faculty_partner_as_gf(self):
+        self.client.force_login(self.user_other_gf)
+        url = reverse('partnerships:partners:update', kwargs={'pk': self.partner_gf.pk})
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'partnerships/partner_update.html')
 
     def test_post(self):
         self.client.force_login(self.user_adri)
