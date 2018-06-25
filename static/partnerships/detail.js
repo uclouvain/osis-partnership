f7ffdd92d79b8c58578a6da68d3c60107f4c8064
Gagaro <gagaro42@gmail.com>
2018-06-25 09:59:45 +0200

    partner entity add/update

--- static/partnerships/detail.js
+++ static/partnerships/detail.js
@@ -8,8 +8,12 @@
         var $modal = $('#' + $(this).attr('data-modal'));
         var $modal_body = $modal.find('.modal-body');
         var url = $(this).attr('href');
+        var submit = $(this).attr('data-modal-submit');
+        var title = $(this).attr('data-modal-title');
 
         $modal.find('form').attr('action', url);
+        $modal.find('form *[type=submit]').text(submit);
+        $modal.find('.modal-title').text(title);
         $modal_body.html('<i class="fa fa-circle-o-notch fa-spin fa-fw fa-5x"></i>');
         $modal.modal('show');
 
