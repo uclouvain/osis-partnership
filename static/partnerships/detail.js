80a4e251e79184ffc8bd22d46d4dc164fefb908a
Gagaro <gagaro42@gmail.com>
2018-06-27 15:08:03 +0200

    partner media delete

--- static/partnerships/detail.js
+++ static/partnerships/detail.js
@@ -5,16 +5,23 @@
         event.preventDefault();
         event.stopPropagation();
 
-        var $modal = $('#' + $(this).attr('data-modal'));
+        var $modal = $('#' + $(this).data('modal'));
         var $modal_body = $modal.find('.modal-body');
+        var $modal_submit = $modal.find('form *[type=submit]');
         var url = $(this).attr('href');
-        var submit = $(this).attr('data-modal-submit');
-        var title = $(this).attr('data-modal-title');
+        var submit = $(this).data('modal-submit');
+        var title = $(this).data('modal-title');
+        var isDanger = $(this).data('modal-danger') !== undefined;
 
         $modal.find('form').attr('action', url);
-        $modal.find('form *[type=submit]').text(submit);
         $modal.find('.modal-title').text(title);
         $modal_body.html('<i class="fa fa-circle-o-notch fa-spin fa-fw fa-5x"></i>');
+        $modal_submit.text(submit);
+        if (isDanger) {
+            $modal_submit.removeClass('btn-primary').addClass('btn-danger');
+        } else {
+            $modal_submit.removeClass('btn-danger').addClass('btn-primary');
+        }
         $modal.modal('show');
 
         $.get(url, function(data) {
