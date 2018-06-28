8bb4ca1e3cb8ff8281ff4f5071e1207858081f5f
Gagaro <gagaro42@gmail.com>
2018-06-28 17:18:51 +0200

    fix migrations to allow backward migration

--- migrations/0001_initial.py
+++ migrations/0001_initial.py
@@ -199,7 +199,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='partnership',
             name='ucl_university_labo',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='+', to='base.EntityVersion', verbose_name='partner_entity'),
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.PROTECT, related_name='+', to='base.EntityVersion', verbose_name='partner_entity'),
         ),
         migrations.AddField(
             model_name='partner',
