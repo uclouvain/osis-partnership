eaa2c7b4e17e8e2e4ae2daabcec3ddcaa7420a01
Vincent Montécot <vincent.montecot@makina-corpus.com>
2018-09-28 14:20:42 +0200

    added context name in update view, changed url namespace in template and added tests for update view.

--- tests/test_ucl_management_entity_update_view.py
+++ tests/test_ucl_management_entity_update_view.py
@@ -4,6 +4,8 @@ from base.tests.factories.user import UserFactory
 from base.tests.factories.entity import EntityFactory
 from base.tests.factories.person import PersonFactory
 from base.tests.factories.entity_version import EntityVersionFactory
+from base.tests.factories.entity_manager import EntityManagerFactory
+from base.tests.factories.person_entity import PersonEntityFactory
 from base.models.enums.entity_type import FACULTY
 from partnership.tests.factories import UCLManagementEntityFactory
 from django.urls import reverse
@@ -13,8 +15,23 @@ class UCLManagementEntityUpdateViewTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.user = UserFactory()
-        cls.ucl_management_entity = UCLManagementEntityFactory()
+        faculty = EntityFactory()
+        other_faculty = EntityFactory()
+        EntityVersionFactory(entity_type="FACULTY", entity=faculty)
+        EntityVersionFactory(entity_type="FACULTY", entity=other_faculty)
+
+        cls.ucl_management_entity = UCLManagementEntityFactory(faculty=faculty)
+
+        # Users
+        cls.lambda_user = UserFactory()
+        cls.adri_user = UserFactory()
+        entity_version = EntityVersionFactory(acronym="ADRI")
+        PersonEntityFactory(entity=entity_version.entity, person__user=cls.adri_user)
+        cls.gf_user = UserFactory()
+        entity_manager = EntityManagerFactory(person__user=cls.gf_user, entity=faculty)
+        cls.other_gf_user = UserFactory()
+        EntityManagerFactory(person__user=cls.other_gf_user, entity=other_faculty)
+
         cls.url = reverse(
             "partnerships:ucl_management_entities:update",
             kwargs={'pk': cls.ucl_management_entity.pk},
@@ -48,14 +65,63 @@ class UCLManagementEntityUpdateViewTest(TestCase):
         self.assertTemplateUsed(response, 'registration/login.html')
 
     def test_get_view_authenticated(self):
-        self.client.force_login(self.user)
+        self.client.force_login(self.lambda_user)
         response = self.client.get(self.url, follow=True)
-        self.assertTemplateUsed('partnerships/ucl_management_entities/uclmanagemententity_update.html')
+        self.assertTemplateNotUsed(response, 'partnerships/ucl_management_entities/uclmanagemententity_update.html')
+        self.assertEqual(response.status_code, 403)
 
-    def test_post(self):
-        self.client.force_login(self.user)
+    def test_get_view_gf_user(self):
+        self.client.force_login(self.gf_user)
+        response = self.client.get(self.url, follow=True)
+        self.assertTemplateUsed(response, 'partnerships/ucl_management_entities/uclmanagemententity_update.html')
+
+    def test_get_view_other_gf_user(self):
+        self.client.force_login(self.other_gf_user)
+        response = self.client.get(self.url, follow=True)
+        self.assertTemplateNotUsed(response, 'partnerships/ucl_management_entities/uclmanagemententity_update.html')
+        self.assertEqual(response.status_code, 403)
+
+    def test_get_view_adri_user(self):
+        self.client.force_login(self.adri_user)
+        response = self.client.get(self.url, follow=True)
+        self.assertTemplateUsed(response, 'partnerships/ucl_management_entities/uclmanagemententity_update.html')
+
+    def test_post_view_anonymous(self):
         response = self.client.post(self.url, data=self.data, follow=True)
-        self.assertTemplateUsed(response, 'partnerships/ucl_management_entity/uclmanagemententity_detail.html')
+        self.assertTemplateNotUsed(response, 'partnerships/ucl_management_entities/uclmanagemententity_update.html')
+        self.assertTemplateUsed(response, 'registration/login.html')
+
+    def test_post_view_authenticated(self):
+        self.client.force_login(self.lambda_user)
+        response = self.client.post(self.url, data=self.data, follow=True)
+        self.assertTemplateNotUsed(response, 'partnerships/ucl_management_entities/uclmanagemententity_update.html')
+        self.assertEqual(response.status_code, 403)
+
+    def test_post_view_gf_user(self):
+        self.client.force_login(self.gf_user)
+        response = self.client.post(self.url, data=self.data, follow=True)
+        self.assertTemplateUsed(response, 'partnerships/ucl_management_entities/uclmanagemententity_update.html')
+        self.assertEqual(self.ucl_management_entity.academic_responsible, self.data['academic_responsible'])
+        self.assertEqual(self.ucl_management_entity.administrative_responsible, self.data['administrative_responsible'])
+        self.assertEqual(self.ucl_management_entity.contact_in_email, self.data['contact_in_email'])
+        self.assertEqual(self.ucl_management_entity.contact_in_person, self.data['contact_in_person'])
+        self.assertEqual(self.ucl_management_entity.contact_in_url, self.data['contact_in_url'])
+        self.assertEqual(self.ucl_management_entity.contact_out_email, self.data['contact_out_email'])
+        self.assertEqual(self.ucl_management_entity.contact_out_person, self.data['contact_out_person'])
+        self.assertEqual(self.ucl_management_entity.contact_out_url, self.data['contact_out_url'])
+        self.assertEqual(self.ucl_management_entity.faculty, self.data['faculty'])
+        self.assertEqual(self.ucl_management_entity.entity, self.data['entity'])
+
+    def test_post_view_other_gf_user(self):
+        self.client.force_login(self.other_gf_user)
+        response = self.client.post(self.url, data=self.data, follow=True)
+        self.assertTemplateNotUsed(response, 'zpartnerships/ucl_management_entities/uclmanagemententity_update.html')
+        self.assertEqual(response.status_code, 403)
+
+    def test_post_view_adri_user(self):
+        self.client.force_login(self.adri_user)
+        response = self.client.post(self.url, data=self.data, follow=True)
+        self.assertTemplateUsed(response, 'partnerships/ucl_management_entities/uclmanagemententity_update.html')
         self.assertEqual(self.ucl_management_entity.academic_responsible, self.data['academic_responsible'])
         self.assertEqual(self.ucl_management_entity.administrative_responsible, self.data['administrative_responsible'])
         self.assertEqual(self.ucl_management_entity.contact_in_email, self.data['contact_in_email'])
