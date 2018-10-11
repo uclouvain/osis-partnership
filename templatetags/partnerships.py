6ecdc49ea8bf490813b7b099a9aa2d9dbc415586
Gagaro <gagaro42@gmail.com>
2018-10-11 09:21:33 +0200

    hide agreement change button if user does not have permission

--- templatetags/partnerships.py
+++ templatetags/partnerships.py
@@ -11,3 +11,8 @@ def can_change_partner_entity(user, entity):
 @register.filter
 def can_delete_partner_entity(user, entity):
     return entity.user_can_delete(user)
+
+
+@register.filter
+def can_change_agreement(user, agreement):
+    return agreement.user_can_change(user)
