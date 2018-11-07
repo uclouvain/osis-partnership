a5558c898a17cec61ba69abaf3e8804dd356bc02
Gagaro <gagaro42@gmail.com>
2018-11-07 17:06:59 +0100

    add translation for menu

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from django.utils.translation import ugettext_lazy as _
+
+
+# Only used to add the strings in the po files
+_('partnerships_general_configuration')
+_('partnerships_financing_configuration')
