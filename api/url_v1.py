b35b8e6640e9b47814e1896243833300dbddb186
Gagaro <gagaro42@gmail.com>
2019-04-15 17:54:26 +0200

    fix api urls

--- api/url_v1.py
+++ api/url_v1.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from partnership.api.views import ConfigurationView, PartnersListView, PartnershipsListView
 
 urlpatterns = [
-    url(r'^configuration/$', ConfigurationView.as_view(), name=''),
-    url(r'^partners/$', PartnersListView.as_view(), name=''),
-    url(r'^$', PartnershipsListView.as_view(), name=''),
+    url(r'^configuration/$', ConfigurationView.as_view(), name='configuration'),
+    url(r'^partners/$', PartnersListView.as_view(), name='partners'),
+    url(r'^partnerships/$', PartnershipsListView.as_view(), name='parnterships'),
 ]
