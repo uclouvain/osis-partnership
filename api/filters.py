9d6d96945c1a32fc6c524b6895b2d5a061d98034
Gagaro <gagaro42@gmail.com>
2019-04-15 17:54:14 +0200

    partners api supervisor filter

--- api/filters.py
+++ api/filters.py
@@ -1,6 +1,39 @@
+from django import forms
+from django.db.models.expressions import Exists, OuterRef
+from django.db.models.query_utils import Q
+from django.utils.translation import ugettext_lazy as _
 from django_filters import rest_framework as filters
 
-from partnership.models import Partner
+from partnership.models import Partner, UCLManagementEntity
+
+
+class SupervisorFilter(filters.Filter):
+    field_class = forms.UUIDField
+
+    def filter(self, qs, value):
+        return (
+            qs.annotate(
+                has_supervisor_with_entity=Exists(UCLManagementEntity.objects
+                    .filter(
+                        entity__isnull=False,
+                        entity=OuterRef('partnerships__ucl_university_labo'),
+                        faculty=OuterRef('partnerships__ucl_university'),
+                        academic_responsible__uuid=value,
+                    )
+                ), has_supervisor_without_entity=Exists(UCLManagementEntity.objects
+                    .filter(
+                        entity__isnull=True,
+                        faculty=OuterRef('partnerships__ucl_university'),
+                        academic_responsible__uuid=value,
+                    )
+                ))
+                .filter(Q(partnerships__supervisor__uuid=value)
+                        | Q(partnerships__supervisor__isnull=True,
+                            has_supervisor_with_entity=True)
+                        | Q(partnerships__supervisor__isnull=True,
+                            partnerships__ucl_university_labo__isnull=True,
+                            has_supervisor_without_entity=True))
+        )
 
 
 class PartnerFilter(filters.FilterSet):
@@ -27,10 +60,8 @@ class PartnerFilter(filters.FilterSet):
     )
 
     # Must be moved to a specific filter class because FilterSet are too limited
-    supervisor = filters.UUIDFilter(
-        field_name='partnerships__supervisor__uuid', # | 'management_entity__academic_responsible'
-        distinct=True,
-    )
+    supervisor = SupervisorFilter(label=_('partnership_supervisor'))
+
     # Depends on the current year
     # education_field
     # mobility_type
@@ -43,4 +74,5 @@ class PartnerFilter(filters.FilterSet):
             'ordering',
             'continent', 'country', 'city', 'partner',
             'ucl_university', 'ucl_university_labo',
+            'supervisor',
         ]
