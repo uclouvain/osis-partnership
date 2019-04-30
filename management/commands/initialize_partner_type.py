aab64efd39aece706d04c80574640311f62699ec
Sebastien Corbin <sebastien.corbin@makina-corpus.com>
2019-04-30 12:12:30 +0200

    Some pep8 fixes but remode check

--- management/commands/initialize_partner_type.py
+++ management/commands/initialize_partner_type.py
@@ -3,10 +3,11 @@ import csv
 
 from django.core.management import BaseCommand
 
+from partnership.management.commands import ProgressBarMixin
 from partnership.models import Partner
 
 
-class Command(BaseCommand):
+class Command(ProgressBarMixin, BaseCommand):
 
     line = -1
 
@@ -14,28 +15,6 @@ class Command(BaseCommand):
         parser.add_argument(
             'csv_file', type=argparse.FileType('r'), help='partner_types.csv',
         )
-
-    def print_progress_bar(self, iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█'):
-        """
-        From http://stackoverflow.com/a/34325723/2575355
-
-        Call in a loop to create terminal progress bar
-        @params:
-            iteration   - Required  : current iteration (Int)
-            total       - Required  : total iterations (Int)
-            prefix      - Optional  : prefix string (Str)
-            suffix      - Optional  : suffix string (Str)
-            decimals    - Optional  : positive number of decimals in percent complete (Int)
-            length      - Optional  : character length of bar (Int)
-            fill        - Optional  : bar fill character (Str)
-        """
-        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-        filled_length = int(length * iteration // total)
-        bar = fill * filled_length + '-' * (length - filled_length)
-        self.stdout.write('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), ending='\r')
-        # Print New Line on Complete
-        if iteration == total:
-            self.stdout.write('')
 
     def write_error(self, message):
         self.stderr.write('(line {line}) {message}'.format(
@@ -55,7 +34,9 @@ class Command(BaseCommand):
             partner_id = line[0]
             partner_type_id = line[6]
             try:
-                Partner.objects.filter(id=partner_id).update(partner_type_id=partner_type_id)
+                Partner.objects.filter(id=partner_id).update(
+                    partner_type_id=partner_type_id
+                )
             except Exception as e:
                 self.write_error(str(e))
             self.print_progress_bar(i, total)
--- templatetags/partnerships.py
+++ templatetags/partnerships.py
@@ -6,12 +6,18 @@ register = template.Library()
 
 @register.filter
 def partner_media_download_url(partner, media):
-    return reverse('partnerships:partners:medias:download', kwargs={'partner_pk': partner.pk, 'pk': media.pk})
+    return reverse('partnerships:partners:medias:download', kwargs={
+        'partner_pk': partner.pk,
+        'pk': media.pk,
+    })
 
 
 @register.filter
 def partnership_media_download_url(partnership, media):
-    return reverse('partnerships:medias:download', kwargs={'partnership_pk': partnership.pk, 'pk': media.pk})
+    return reverse('partnerships:medias:download', kwargs={
+        'partnership_pk': partnership.pk,
+        'pk': media.pk,
+    })
 
 
 @register.filter
