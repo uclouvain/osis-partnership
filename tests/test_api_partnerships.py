a45d867d1eebdcd225f7dd676ff328fdbb64748e
Alessandro Brugnetti <alessandro.brugnetti@uclouvain.be>
2020-01-20 11:46:31 +0100

    OP-348 Partnership Listview/Retriveview exclude data which are validated for the current_api_year_configuration

--- tests/test_api_partnerships.py
+++ tests/test_api_partnerships.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from base.tests.factories.academic_year import AcademicYearFactory
 from base.tests.factories.entity import EntityFactory
 from base.tests.factories.person import PersonFactory
-from partnership.models import PartnershipConfiguration
+from partnership.models import PartnershipConfiguration, PartnershipAgreement
 from partnership.tests.factories import PartnershipFactory, PartnerFactory, UCLManagementEntityFactory, \
     PartnershipYearFactory, PartnershipAgreementFactory, PartnershipYearEducationFieldFactory, FinancingFactory
 from reference.models.continent import Continent
@@ -49,6 +49,7 @@ class PartnershipApiViewTest(TestCase):
             partnership=cls.partnership,
             start_academic_year=current_academic_year,
             end_academic_year__year=current_academic_year.year + 1,
+            status=PartnershipAgreement.STATUS_VALIDATED
         )
 
         partnership = PartnershipFactory(supervisor=None, years=[])
@@ -57,6 +58,7 @@ class PartnershipApiViewTest(TestCase):
             partnership=partnership,
             start_academic_year=current_academic_year,
             end_academic_year__year=current_academic_year.year + 1,
+            status=PartnershipAgreement.STATUS_VALIDATED
         )
         cls.management_entity = UCLManagementEntityFactory(
             faculty=partnership.ucl_university,
@@ -66,51 +68,63 @@ class PartnershipApiViewTest(TestCase):
         cls.financing = FinancingFactory(academic_year=current_academic_year)
         cls.financing.countries.add(partnership.partner.contact_address.country)
 
-        # Some noises
+        # Some noises - Agreement not validated or not in status validated
         PartnerFactory()
-        PartnershipFactory()
+        cls.partnership_without_agreement = PartnershipFactory()
+
+        cls.partnership_with_agreement_not_validated = PartnershipFactory(supervisor=None, years=[])
+        PartnershipYearFactory(
+            partnership=cls.partnership_with_agreement_not_validated,
+            academic_year=current_academic_year,
+        )
+        PartnershipAgreementFactory(
+            partnership=cls.partnership_with_agreement_not_validated,
+            start_academic_year=current_academic_year,
+            end_academic_year__year=current_academic_year.year + 1,
+            status=PartnershipAgreement.STATUS_WAITING
+        )
 
     def test_get(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         data = response.json()
-        self.assertEqual(len(data['results']), 3)
+        self.assertEqual(len(data['results']), 2)
 
     def test_ordering_partner(self):
         response = self.client.get(self.url + '?ordering=partner')
         self.assertEqual(response.status_code, 200)
         data = response.json()
-        self.assertEqual(len(data['results']), 3)
+        self.assertEqual(len(data['results']), 2)
 
     def test_ordering_country_en(self):
         response = self.client.get(self.url + '?ordering=country_en')
         self.assertEqual(response.status_code, 200)
         data = response.json()
-        self.assertEqual(len(data['results']), 3)
+        self.assertEqual(len(data['results']), 2)
 
     def test_ordering_city(self):
         response = self.client.get(self.url + '?ordering=city')
         self.assertEqual(response.status_code, 200)
         data = response.json()
-        self.assertEqual(len(data['results']), 3)
+        self.assertEqual(len(data['results']), 2)
 
     def test_ordering_ucl_university(self):
         response = self.client.get(self.url + '?ordering=ucl_university')
         self.assertEqual(response.status_code, 200)
         data = response.json()
-        self.assertEqual(len(data['results']), 3)
+        self.assertEqual(len(data['results']), 2)
 
     def test_ordering_type(self):
         response = self.client.get(self.url + '?ordering=type')
         self.assertEqual(response.status_code, 200)
         data = response.json()
-        self.assertEqual(len(data['results']), 3)
+        self.assertEqual(len(data['results']), 2)
 
     def test_ordering_subject_area(self):
         response = self.client.get(self.url + '?ordering=subject_area')
         self.assertEqual(response.status_code, 200)
         data = response.json()
-        self.assertEqual(len(data['results']), 3)
+        self.assertEqual(len(data['results']), 2)
 
     def test_filter_continent(self):
         response = self.client.get(self.url + '?continent=' + str(self.continent.name))
@@ -178,10 +192,26 @@ class PartnershipApiViewTest(TestCase):
         data = response.json()
         self.assertEqual(len(data['results']), 1)
 
-    def test_retrieve(self):
+    def test_retrieve_case_partnership_with_agreement_validated(self):
         url = reverse(
             'partnership_api_v1:partnerships:retrieve',
             kwargs={'uuid': self.partnership.uuid},
         )
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+
+    def test_retrieve_case_partnership_without_agreement(self):
+        url = reverse(
+            'partnership_api_v1:partnerships:retrieve',
+            kwargs={'uuid': self.partnership_without_agreement.uuid},
+        )
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_retrieve_case_partnership_with_agreement_not_validated(self):
+        url = reverse(
+            'partnership_api_v1:partnerships:retrieve',
+            kwargs={'uuid': self.partnership_with_agreement_not_validated.uuid},
+        )
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
