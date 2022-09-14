a2c042adeb450d3f75676e16a296fc359228d178
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2022-09-14 12:17:35 +0200

    add PartnershipPartnerRelationFactory

---
+++ tests/factories/parternship_partner_relation.py
@@ -0,0 +1,15 @@
+import factory
+
+from partnership.models import PartnershipPartnerRelation
+from . import PartnershipFactory
+
+
+class PartnershipPartnerRelationFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = PartnershipPartnerRelation
+
+    partnership = factory.SubFactory(PartnershipFactory)
+    entity = factory.SubFactory(
+        'base.tests.factories.entity.EntityWithVersionFactory',
+        version__acronym="SO"
+    )
