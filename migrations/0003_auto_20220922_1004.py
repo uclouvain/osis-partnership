c0c7a2185ed7c8032bf1bd73424bf4248134bca2
unknown <alessandro.brugnetti@uclouvain.be>
2022-09-28 10:43:49 +0200

    OP-502: Migration des liens alfresco vers sharepoint

--- migrations/0003_auto_20220922_1004.py
+++ migrations/0003_auto_20220922_1004.py
@@ -27,10 +27,29 @@ urls_to_migrate = [
         'from': 'https://alfresco.uclouvain.be/alfresco/webdav/UCL/Services généraux/ADRI/UCL_personnel/eval2017/',
         'to': 'https://uclouvain.sharepoint.com/sites/RFM/Partenariats/Archivage/Evaluations/',
     },
+    {
+        'from': 'https://alfresco.uclouvain.be/alfresco/webdav/UCL/Services généraux/ADRI/UCL_personnel/eval2013/',
+        'to': 'https://uclouvain.sharepoint.com/sites/RFM/Partenariats/Archivage/Evaluations/2013/',
+    },
+    {
+        'from': 'https://alfresco.uclouvain.be/alfresco/webdav/UCL/Services généraux/ADRI/UCL_personnel/eval/',
+        'to': 'https://uclouvain.sharepoint.com/sites/RFM/Partenariats/Archivage/Evaluations/2010/',
+    },
+
     # Flux migration
     {
         'from': 'https://alfresco.uclouvain.be/alfresco/webdav/UCL/Services%20g%C3%A9n%C3%A9raux/ADRI/UCL_personnel_et_etudiants/flux_2020/',
         'to': 'https://uclouvain.sharepoint.com/sites/RFM/Partenariats/Flux/',
+    },
+
+    # Stat BW
+    {
+        'from': 'https://alfresco.uclouvain.be/alfresco/webdav/UCL/Services%20g%C3%A9n%C3%A9raux/ADRI/ucl_personnel/stats_bw/CA%20FREDERI01_2018-2013.pdf',
+        'to': 'https://uclouvain.sharepoint.com/sites/RFM/Partenariats/Flux/CA%20FREDERI01_2018-2013.pdf'
+    },
+    {
+        'from': 'https://alfresco.uclouvain.be/alfresco/webdav/UCL/Services%20g%C3%A9n%C3%A9raux/ADRI/UCL_personnel/eval/A%20%20WIEN01.pdf',
+        'to': 'https://uclouvain.sharepoint.com/sites/RFM/Partenariats/Archivage/Evaluations/A%20%20WIEN01.pdf'
     }
 ]
 
