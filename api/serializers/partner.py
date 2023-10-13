d316aedf5e220a0cce6c298528e06034b9e16622
Gagaro <gagaro42@gmail.com>
2023-10-13 15:07:19 +0200

    Add API / fields for internships. (RAD Outsystems)

--- api/serializers/partner.py
+++ api/serializers/partner.py
@@ -1,13 +1,23 @@
+import datetime
 import json
+
+from django.db import transaction
 from rest_framework import serializers
 
-from partnership.models import Partner
+from base.models.entity_version import EntityVersion
+from base.models.entity_version_address import EntityVersionAddress
+from base.models.enums.organization_type import ORGANIZATION_TYPE
+from base.models.organization import Organization
+from partnership.models import Partner, EntityProxy
 
 __all__ = [
     'PartnerListSerializer',
     'PartnerAdminSerializer',
     'PartnerDetailSerializer',
 ]
+
+from partnership.utils import generate_partner_prefix
+from reference.models.country import Country
 
 
 class PartnerListSerializer(serializers.ModelSerializer):
@@ -58,3 +68,98 @@ class PartnerAdminSerializer(PartnerListSerializer):
             'name', 'erasmus_code', 'partner_type', 'city', 'country',
             'partnerships_count', 'is_actif', 'is_valid', 'url', 'id',
         ]
+
+
+class InternshipPartnerSerializer(serializers.ModelSerializer):
+    type = serializers.ChoiceField(choices=ORGANIZATION_TYPE, source="organization.type")
+    name = serializers.CharField(max_length=255, source="organization.name")
+    website = serializers.CharField(max_length=255, source="entity.website")
+    street_number = serializers.CharField(max_length=12, required=False, source="contact_address.street_number")
+    street = serializers.CharField(max_length=255, source="contact_address.street")
+    postal_code = serializers.CharField(max_length=32, required=False, source="contact_address.postal_code")
+    city = serializers.CharField(max_length=255, source="contact_address.city")
+    country = serializers.CharField(max_length=2, source="contact_address.country")
+    latitude = serializers.FloatField(min_value=-90, max_value=90, required=False)
+    longitude = serializers.FloatField(min_value=-180, max_value=180, required=False)
+
+    # Read only
+    start_date = serializers.CharField(source="organization.start_date", read_only=True)
+    end_date = serializers.CharField(source="organization.end_date", read_only=True)
+
+    class Meta:
+        model = Partner
+        fields = [
+            'uuid', 'is_valid', 'organisation_identifier', 'size', 'is_public', 'is_nonprofit',
+            'erasmus_code', 'pic_code', 'contact_type', 'phone', 'email',
+            # Organization
+            'name', 'type',
+            # Entity
+            'website', 'start_date', 'end_date',
+            # EntityAddress
+            'street_number', 'street', 'postal_code', 'city', 'country',
+            'latitude', 'longitude',
+        ]
+        read_only_fields = [
+            'is_valid', 'start_date', 'end_date', 'erasmus_code', 'pic_code', 'phone', 'email', 'contact_type',
+        ]
+
+    def validate_country(self, value):
+        try:
+            return Country.objects.get(iso_code__iexact=value)
+        except Country.DoesNotExist:
+            raise serializers.ValidationError("Country not found")
+
+    @transaction.atomic()
+    def create(self, validated_data):
+        # Create organization
+        organization = Organization.objects.create(
+            name=validated_data['organization']['name'],
+            type=validated_data['organization']['type'],
+            prefix=generate_partner_prefix(validated_data['organization']['name'])
+        )
+
+        # Entity
+        entity = EntityProxy.objects.create(
+            website=validated_data['entity']['website'],
+            organization=organization,
+        )
+
+        # EntityVersion
+        last_version = EntityVersion.objects.create(
+            title=entity.organization.name,
+            entity=entity,
+            acronym=entity.organization.prefix,
+            parent=None,
+            start_date=datetime.date.today(),
+        )
+
+        # Partner
+        partner = Partner.objects.create(
+            organisation_identifier=validated_data.get('organisation_identifier', ''),
+            size=validated_data['size'],
+            is_public=validated_data['is_public'],
+            is_nonprofit=validated_data['is_nonprofit'],
+            organization=organization,
+            author=self.context['request'].user.person,
+        )
+
+        # EntityVersionAddress
+        longitude = validated_data['contact_address'].get("longitude")
+        latitude = validated_data['contact_address'].get("latitude")
+        if latitude and longitude:
+            location = f'SRID=4326;POINT({longitude} {latitude})'
+        else:
+            location = None
+        EntityVersionAddress.objects.create(
+            entity_version=last_version,
+            is_main=True,
+            street_number=validated_data['contact_address'].get('street_number', ''),
+            street=validated_data['contact_address'].get('street', ''),
+            postal_code=validated_data['contact_address'].get('postal_code', ''),
+            state=validated_data['contact_address'].get('state', ''),
+            city=validated_data['contact_address'].get('city', ''),
+            country=validated_data['contact_address'].get('country'),
+            location=location,
+        )
+
+        return partner
