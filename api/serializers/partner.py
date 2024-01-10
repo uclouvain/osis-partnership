4b14ede074d96a9faa4e62d3433b771eee8d3597
unknown <alessandro.brugnetti@uclouvain.be>
2024-01-10 10:30:20 +0100

    Remove default value for organization_identifier in InternshipPartnership

--- api/serializers/partner.py
+++ api/serializers/partner.py
@@ -147,7 +147,7 @@ class InternshipPartnerSerializer(serializers.ModelSerializer):
 
         # Partner
         partner = Partner.objects.create(
-            organisation_identifier=validated_data.get('organization_identifier', ''),
+            organisation_identifier=validated_data.get('organization_identifier'),
             size=validated_data['size'],
             is_public=validated_data['is_public'],
             is_nonprofit=validated_data['is_nonprofit'],
