e859b50e5d006b0bf3dafc0bcdabca4fc20c260b
Anne laurence Dermine <annelaurence.dermine@usaintlouis.be>
2024-07-11 15:55:07 +0200

    osis-9265 : partnership model

--- models/enums/partnership.py
+++ models/enums/partnership.py
@@ -1,5 +1,4 @@
 from django.utils.translation import gettext_lazy as _
-
 from base.models.utils.utils import ChoiceEnum
 
 
@@ -19,3 +18,30 @@ class PartnershipType(ChoiceEnum):
             PartnershipType.COURSE.name,
             PartnershipType.DOCTORATE.name,
         ]
+
+class PartnershipDiplomaWithUCL(ChoiceEnum):
+    UNIQUE = _('Partenaire unique')
+    SEPARED = _('Partenaire séparé')
+    NO_CODIPLOMA = _('Non co-diplômant')
+
+    @classmethod
+    def get_list_names(cls):
+        return [
+            PartnershipDiplomaWithUCL.UNIQUE.name,
+            PartnershipDiplomaWithUCL.SEPARED.name,
+            PartnershipDiplomaWithUCL.NO_CODIPLOMA.name
+        ]
+
+class PartnershipProductionSupplement(ChoiceEnum):
+    YES = _('Oui')
+    NO = _('Non')
+    SHARED = _('Partagé')
+
+    @classmethod
+    def get_list_names(cls):
+        return [
+            PartnershipProductionSupplement.YES.name,
+            PartnershipProductionSupplement.NO.name,
+            PartnershipProductionSupplement.SHARED.name
+        ]
+
--- models/partnership/__init__.py
+++ models/partnership/__init__.py
@@ -4,10 +4,11 @@ try:
     from .partnership import *
     from .partnership_year import *
     from .partnership_year_education import *
+    from .partnershippartnerrelation import *
 
     # Prevent polluting the namespace with module names
     for name in ['agreement', 'configuration', 'partnership',
-                 'partnership_year', 'partnership_year_education']:
+                 'partnership_year', 'partnership_year_education', 'partnershippartnerrelation']:
         del globals()[name]
 except RuntimeError as e:  # pragma: no cover
     # There's a weird bug when running tests, the test runner seeing a models
