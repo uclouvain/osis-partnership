b5814fb388e92f6d08bc92ad708de10eff470154
Anne laurence Dermine <annelaurence.dermine@usaintlouis.be>
2025-03-07 09:10:45 +0100

    op-528 : review code

--- forms/partnership/partnership.py
+++ forms/partnership/partnership.py
@@ -306,21 +306,3 @@ class PartnershipProjectForm(PartnershipWithDatesMixin):
             'project_title',
             'ucl_status',
         )
-
-
-# class PartnershipPartnerRelationForm(forms.ModelForm):
-#     def __init__(self, data=None, files=None, auto_id='id_%s', prefix=None, initial=None, error_class=ErrorList,
-#                  label_suffix=None, empty_permitted=False, instance=None, use_required_attribute=None, renderer=None):
-#         super().__init__(data, files, auto_id, prefix, initial, error_class, label_suffix, empty_permitted, instance,
-#                          use_required_attribute, renderer)
-#
-#     class Meta:
-#         model = PartnershipPartnerRelation
-#         exclude = ['id']
-#
-#
-# PartnershipPartnerRelationFormSet = modelformset_factory(
-#     PartnershipPartnerRelation,
-#     form=PartnershipPartnerRelationForm,
-#     extra=0
-# )
