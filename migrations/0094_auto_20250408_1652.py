9267470dcf0c74fcdf514458cee03bfa468f0b70
Anne laurence Dermine <annelaurence.dermine@usaintlouis.be>
2025-04-08 16:52:48 +0200

    op-530 : partnership course

--- migrations/0094_auto_20250408_1652.py
+++ migrations/0094_auto_20250408_1652.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('partnership', '0011_remove_partnership_all_student_and_more'),
+        ('partnership', '0093_partnershipyear_flow_direction'),
     ]
 
     state_operations = [
