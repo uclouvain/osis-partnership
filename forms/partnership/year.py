aa28b5726851d649ed8b61fc04ca4fa7842cd7c6
Anne laurence Dermine <annelaurence.dermine@usaintlouis.be>
2025-05-16 16:06:54 +0200

    OP-530 : update model

--- forms/partnership/year.py
+++ forms/partnership/year.py
@@ -282,7 +282,7 @@ class PartnershipYearCourseForm(PartnershipYearWithoutDatesForm):
             if current_academic_year is not None:
                 past_academic_years = AcademicYear.objects.all()
                 future_academic_years = AcademicYear.objects.filter(
-                    year__gte= current_academic_year.year
+                    year__gte=current_academic_year.year
                 )
                 if 'start_academic_year' in self.fields:
                     self.fields['start_academic_year'].queryset = past_academic_years
