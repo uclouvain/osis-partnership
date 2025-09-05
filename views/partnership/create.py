3e76ed76a440b2ab6ed1c73d811e36951b1d3baf
Anne laurence Dermine <annelaurence.dermine@usaintlouis.be>
2025-09-05 10:16:58 +0200

    OP-533 & OSIS-10163 : changement métier

--- views/partnership/create.py
+++ views/partnership/create.py
@@ -60,6 +60,15 @@ class PartnershipCreateView(NotifyAdminMailMixin,
         self.partnership_type = self.kwargs['type'].name
         return self.kwargs['type']
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.partnership_type == PartnershipType.COURSE.name:
+            self.bool_partnership_type_course = True
+        else:
+            self.bool_partnership_type_course = False
+        context["bool_partnership_type_course"] =  self.bool_partnership_type_course
+        return context
+
     @transaction.atomic
     def form_valid(self, form, form_year):
         partnership = form.save(commit=False)
